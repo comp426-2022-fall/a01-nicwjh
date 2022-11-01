2621f82cbc39ba19b7442936006607324b7bdf7f
Nicholas Wong <69724812+nicwjh@users.noreply.github.com>
2022-11-01 01:40:47 -0400

    test

--- server.js
+++ server.js
@@ -9,22 +9,26 @@ var minimist = require('minimist');
 
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
 const args = minimist(process.argv.slice(2));
+
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
-if (args[0] == null){ port = 3000;}
-else{port = args[0];}
+const port = args['_'][0] | 3000; 
+console.log(port);
+
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
 // The stuff that should be inside this function is all below.
+
 fs.readFile('./public/index.html', 'utf8', (err, data) => {
 console.log("readfile called");
 if (err){
 console.error(err);
 return;
 }
-console.log(data);
+//console.log(data);
 });
+
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
 
@@ -43,7 +47,7 @@ res.setHeader('Content-Type','text/html');
 res.end(req);
 }).listen(port);
 
-console.log('Server listening on port ${port}');
+console.log(`Server listening on port ${port}`);
 
 
 // Start the `server` const listening on the port defined by argument in your `port` const. 
