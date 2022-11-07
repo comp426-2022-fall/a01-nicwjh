74321b9fcdc81b451e1666241552681ddd913e8c
Nicholas Wong <69724812+nicwjh@users.noreply.github.com>
2022-11-07 03:45:06 -0500

    test

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const args = minimist(process.argv.slice(2));
 
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
-const port = args.port;
+const port = args.port || 3000;
 console.log(args.port);
 
 
