c01686d8f7a41ecf97f3299ba80ffdc1237cdc08
Nicholas Wong <69724812+nicwjh@users.noreply.github.com>
2022-11-07 15:54:36 -0500

    test

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ console.log(data);
 const server = http.createServer((req,res)=>{
 res.setHeader('Content-Type','text/html');
 res.writeHead(200);
-res.send(data_global);
+res.end(data_global);
 });
 
 server.listen(port, () => { 
