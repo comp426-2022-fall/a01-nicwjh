f4a0372a7d465573a3d43306964f815de3399305
Nicholas Wong <69724812+nicwjh@users.noreply.github.com>
2022-11-06 23:31:18 -0500

    constants

--- server.js
+++ server.js
@@ -2,10 +2,10 @@
 const http = require("http");
 
 // Require fs module
-var fs = require("fs");
+const fs = require("fs");
 
 // Require minimist module (make sure you install this one via npm).
-var minimist = require('minimist');
+const minimist = require('minimist');
 
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
 const args = minimist(process.argv.slice(2));
