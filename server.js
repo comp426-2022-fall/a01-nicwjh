b42fcd5449907b0fd4440bf602f26d0757d56bb6
Nicholas Wong <69724812+nicwjh@users.noreply.github.com>
2022-11-07 00:21:02 -0500

    test

--- server.js
+++ server.js
@@ -39,9 +39,9 @@ console.log(data);
 // 3. end with the data that you are reading in from ./public/index.html.
 
 const server = http.createServer((req,res)=>{
-res.writeHead(200);
 res.setHeader('Content-Type','text/html');
 res.send(data);
+res.writeHead(200);
 });
 
 server.listen(port, () => { 
