f9fa32cae7a61fb81b91686fd3541d4d81c5e3c5
Nicholas Wong <69724812+nicwjh@users.noreply.github.com>
2022-11-06 23:19:47 -0500

    definitely fixed port

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ const args = minimist(process.argv.slice(2));
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
 const port = args.port || 3000;
-console.log(process.argv[2]);
-console.log(args);
-console.log(port);
+console.log(args.port
+console.log(args)
+console.log(port)
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
